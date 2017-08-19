3e01710164125d8d1cc79a02efe9a8ccffa765d3
cesar <heellxz@gmail.com>
2017-08-19 04:57:50 -0300

    nodo al digitalocean

--- src/net.cpp
+++ src/net.cpp
@@ -1173,12 +1173,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"chauchanodo1", "152.172.211.159"},
+    {"chauchanodo1", "174.138.59.111"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"chauchanodo1", "152.172.211.159"},
+    {"chauchanodo1", "174.138.59.111"},
     {NULL, NULL}
 };
 
