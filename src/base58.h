00cadbcabe30669d7fec95e3d050d418a4932a0e
Cesar Vasquez <heellxz@gmail.com>
2017-08-19 01:19:07 -0400

    pubkey para address que empiecen con c

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Chaucha addresses start with L
+        PUBKEY_ADDRESS = 88, // Chaucha addresses start with L
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
